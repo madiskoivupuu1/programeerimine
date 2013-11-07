e5e66e8b62c73a2a3c17efb58f28f6e84a5e56d8
reinp <reinpri@hotmail.com>
2013-11-07 15:56:18 +0200

    arvutuse lisamine

---
+++ arvutused.py
@@ -0,0 +1,33 @@
+import math
+
+print "risttahukas"
+a = 10
+b = 15
+c = 20
+servad = 4 * (a+b+c)
+print "servad={}".format(servad)
+pindala= 2 * (a*b + b*c +a*c)
+print"pindala={}".format(pindala)
+ruumala= a * b * c
+print "ruumala={}".format(ruumala)
+
+# Pyramiid
+print "Pyramiid"
+a = 12
+h = 7
+c = math.sqrt (2*a*a+h*h)
+servad = 4*a*c
+print "servad={}".format(servad)
+pindala= Sp + Sk
+Sp= 4*a
+Sk= Sp*h/7
+St= Sp + Sk
+Ruumala= Sp*h/3
+print "ruumala={}".format(ruumala)
+
+
+
+
+
+
+ 
